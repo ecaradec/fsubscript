c55aed58ae6eae07eb8693d7056bf362f52713e4
ecaradec <emmanuel.caradec@gmail.com>
2008-11-30 18:01:27 +0100

    ewemoa v1

--- fscript.js
+++ fscript.js
@@ -1,7 +1,7 @@
 // plugin script :
 displayname="FARRSubScript";
-versionstring="0.9.0";
-releasedatestring="Jan 1st, 2008";
+versionstring="0.9.1";
+releasedatestring="Nov 18th, 2008";
 author="Author";
 updateurl="";
 homepageurl="";
@@ -26,149 +26,191 @@ STOPPED=0; SEARCHING=1;
 
 HANDLED=1; CLOSE=2;
 
-function setStatusBar(txt) { }
-function setSearch(txt) { FARR.setStrValue("setsearch",txt); }
-function setSearchNoGo() { FARR.setStrValue("setsearchnogo",txt); }
-function stopSearch() { }
-function hideWindow() { }
-function showWindow() { }
-function toggleWindow() { }
-function setRichEditMode() { }
-function setRichEditHeight() { }
-function setRichEditWidth() { }
-function setShowAllMode() { }
-function exit() { }
-function reportError(txt) { }
-function setCliboard() { }
+function setStatusBar(txt) { FARR.setStrValue("statusbar", txt); }
+function setSearch(txt) { FARR.setStrValue("setsearch", txt); }
+function setSearchNoGo() { FARR.setStrValue("setsearchnogo", txt); }
+function stopSearch() { FARR.setStrValue("stopsearch"); }
+function hideWindow() { FARR.setStrValue("window.hide"); }
+function showWindow() { FARR.setStrValue("window.show"); }
+function toggleWindow() { FARR.setStrValue("window.toggle"); }
+function setRichEditMode(txt) { FARR.setStrValue("window.richeditmode", txt); }
+function setRichEditHeight(height) { FARR.setStrValue("window.richeditheight", height); }
+function setRichEditWidth(width) { FARR.setStrValue("window.richeditwidth", width); }
+function setShowAllMode() { FARR.setStrValue("setshowallmode"); }
+function exit() { FARR.setStrValue("exit"); }
+function reportError(txt) { FARR.setStrValue("reporterror", txt); }
+function setCliboard() { FARR.setStrValue("clipboard"); }
 function pasteClipboardToLastActiveWindow(txt) { FARR.setStrValue("PasteClipboardToLastActiveWindow", txt) }
-function displayAlertMessage(txt) { FARR.setStrValue("DisplayAlertMessage",txt); }
-function displayAlertMessageNoTimeout() { }
-function displayBalloonMessage() { }
-function execWebbrowserEmbededJavascript() { }
+function displayAlertMessage(txt) { FARR.setStrValue("DisplayAlertMessage", txt); }
+function displayAlertMessageNoTimeout(txt) { FARR.setStrValue("DisplayAlertMessageNoTimeout", txt); }
+function displayBalloonMessage(txt) { FARR.setStrValue("DisplayBalloonMessage", txt); }
+function execWebbrowserEmbededJavascript(txt) { FARR.setStrValue("EmbeddedWb.ExecJavascript", txt); }
 
-function restartSearch(txt) { FARR.setStrValue("launch","restartsearch "+txt); }
-function doSearch(txt) { FARR.setStrValue("launch","dosearch "+txt); }
-function doSearchOnTrigger(txt) { FARR.setStrValue("launch","dosearchontrigger "+txt); }
+function paste(txt) { FARR.setStrValue("launch", "paste " + txt); }
+function copyClip(txt) { FARR.setStrValue("launch", "copyclip " + txt); }
+function restartSearch(txt) { FARR.setStrValue("launch", "restartsearch " + txt); }
+function doSearch(txt) { FARR.setStrValue("launch", "dosearch " + txt); }
+function doSearchOnTrigger(txt) { FARR.setStrValue("launch", "dosearchontrigger " + txt); }
+// txt: text with \n as newlines
+function showMemo(txt) { FARR.setStrValue("launch", "showmemo " + txt); }
+function showFile(filename) { FARR.setStrValue("launch", "showfile " + filename); }
+function showFileRTF(filename) { FARR.setStrValue("launch", "showfilertf " + filename); }
+function showFileHTML(filename) { FARR.setStrValue("launch", "showfilehtml " + filename); }
+function showHTML(txt) { FARR.setStrValue("launch", "showhtml " + txt); }
+// txt: url
+function htmlViewURL(txt) { FARR.setStrValue("launch", "htmlviewurl " + txt); }
+// txt: searchedit, mainpanel
+function setFocus(txt) { FARR.setStrValue("launch", "setfocus " + txt); }
+// txt: list, memo, html, user, spreadsheet
+function setViewMode(txt) { FARR.setStrValue("launch", "setviewmode " + txt); } 
+function pcommand(txt) { FARR.setStrValue("launch", "pcommand " + txt); }
+function setSize(minwidth, minheight, maxwidth, maxheight) { 
+  FARR.setStrValue("launch", "setsize " +
+                   minwidth + ", " + minheight + ", " +
+                   maxwidth + ", " + maxheight);
+}
+// XXX: bool: 0 or 1?
+function setHTMLSafe(bool) { FARR.setStrValue("launch", "sethtmlsafe " + bool); } 
+function showPleaseWait(txt) { FARR.setStrValue("launch", "showpleasewait " + txt); }
+function hidePleaseWait() { FARR.setStrValue("launch", "hidepleasewait"); }
+//function alert(txt) { FARR.setStrValue("launch", "alert " + txt); }
+function sleep(ms) { FARR.setStrValue("launch", "sleep " + ms); }
 
 function error(txt) {
-    displayAlertMessage(txt);
+  displayAlertMessage(txt);
 }
 
-plugins={}
+var plugins = {}
 
-var fso=new ActiveXObject("Scripting.FileSystemObject");
+var fso = new ActiveXObject("Scripting.FileSystemObject");
 function getTextFile(path) {
-    var f=fso.OpenTextFile(path);
-    var txt=f.ReadAll();
-    f.Close();
-    return txt;
+  var f = fso.OpenTextFile(path);
+  var txt = f.ReadAll();
+  f.Close();
+  return txt;
 }
 
 var g_currentDirectory;
 function onInit(directory) {
-    g_currentDirectory=directory;
-    var f = fso.GetFolder(directory+"\\..");
-    var fc = new Enumerator(f.SubFolders);
-    var tmp="";
-    for (; !fc.atEnd(); fc.moveNext())
-    {
-        try {
-            var currentDirectory=fc.item();
-            if(fso.FileExists(fc.item()+"\\fsubscript.js"))
-                eval(getTextFile(fc.item()+"\\fsubscript.js"))
-        } catch(e) {
-            error("error occured while loading "+fc.item().Name+"\\fsubscript.js\n"+e);
-        }
-    }
-}
-// mjet : multiple javascript extension for farr
-function onSearchBegin(querykey, explicit, queryraw, querynokeyword, modifier, triggermethod) {
-    FARR.setState(querykey,SEARCHING);
-    var exactMatch=false;
-
-    for(var i in plugins) {
-        try {
-            var isExplicit=queryraw.indexOf(aliasstr+" +"+i)==0;
-            if(isExplicit) exactMatch=true;
-            plugins[i].search(querykey, isExplicit, queryraw, modifier, triggermethod);
-        } catch(e) { error("plugin "+i+" has failed on search : "+e); }
-        //FARR.emitResult(querykey,i, "scripter "+i, iconfilename,UNKNOWN,IMMEDIATE_DISPLAY,1000);
-    }
-    if(!exactMatch && queryraw.indexOf(aliasstr)==0) {
-        var m=queryraw.match(new RegExp("^"+aliasstr+" ?(.*)"));
-        for(var i in plugins) {
-            if(i.indexOf(m[1])!=-1)
-                FARR.emitResult(querykey,i, plugins[i].autocomplete || (aliasstr+" +"+i), plugins[i].icon || iconfilename,ALIAS,MATCH_AGAINST_SEARCH,10000, plugins[i].autocomplete || (aliasstr+" +"+i));
-        }
-    }
-    if(queryraw=="aplugins") {
-        for(var i in plugins) {
-            FARR.emitResult(querykey,(plugins[i].displayName || i) + " ("+plugins[i].version+" - "+plugins[i].lastChange+")", plugins[i].autocomplete || (aliasstr+" +"+i), plugins[i].icon || iconfilename, ALIAS, MATCH_AGAINST_SEARCH,99, plugins[i].autocomplete || (aliasstr+" +"+i));
-        }
-    }
-	FARR.setState(querykey,STOPPED);
-}
-
-function onProcessTrigger(path, title, groupname, pluginid, thispluginid, score, entrytype, args) {
-    for(var i in plugins) {
-        try {
-            var r=plugins[i].trigger(path, title, groupname, pluginid, thispluginid, score, entrytype, args);
-            if(r&3!=0)
-                return r; // stop if stop or closed is required
-        } catch(e) { error("plugin "+i+" has failed on trigger : "+e) }
-    }
-}
-function onReceiveKey(key, altpressed, controlpressed, shiftpressed) {
-    for(var i in plugins) {
-        try {
-            var r=plugins[i].receivekey(key, altpressed, controlpressed, shiftpressed);
-            if(r)
-                return true;
-        } catch(e) { error("plugin "+i+" has failed on received key : "+e) }
-    }
-}
-function onWin32Message(wparam,lparam) {
-    var SHOW_SUBPLUGIN_CONFIG=0;
-    var RELOAD_SUBPLUGINCONFIG=1;
-    if(wparam==SHOW_SUBPLUGIN_CONFIG) {
-        FARR.debug(""+lparam);
-        var index=0;
-        for(var i in plugins) {
-            if(index==lparam-1)
-                break;
-            index++;
-        }
-        plugins[i].showSettings();
-    } else if(wparam==RELOAD_SUBPLUGINCONFIG) {
-        var index=0;
-        for(var i in plugins) {
-            if(index==lparam-1)
-                break;
-            index++;
-        }
-        plugins[i].settingsChanged();
-    }
-}
-function onDoAdvConfig() {  
+  g_currentDirectory = directory;
+  var f = fso.GetFolder(directory + "\\..");
+  var fc = new Enumerator(f.SubFolders);
+  var tmp = "";
+  for (; !fc.atEnd(); fc.moveNext())
+  {
     try {
-        fso.DeleteFile(g_currentDirectory+"\\FSSCSettings.ini");
+      var currentDirectory = fc.item();
+      if (fso.FileExists(fc.item() + "\\fsubscript.js")) {
+        eval(getTextFile(fc.item() + "\\fsubscript.js"));
+      }
     } catch(e) {
+      error("error occured while loading " + fc.item().Name + "\\fsubscript.js\n" + 
+            "message: " + e.message + " " + "name: " + e.name);
     }
-   
-    // extract plugins data for the settings
-    var index=1;
-    for(var i in plugins) {
-        FARR.setIniValue(g_currentDirectory+"\\FSSCSettings.ini", index,"PluginName",i);
-        FARR.setIniValue(g_currentDirectory+"\\FSSCSettings.ini", index,"Icon",plugins[i].icon);
-        index++;
-    }
-
-    FARR.exec(g_currentDirectory+"\\FSSCSettings.exe", "", g_currentDirectory);
-    return true;
+  }
 }
+// 
+function onSearchBegin(querykey, explicit, queryraw, querynokeyword, 
+                       modifier, triggermethod) {
+  FARR.setState(querykey, SEARCHING);
+  if (queryraw == "aplugins") {
+    for (var i in plugins) {
+      var title = (plugins[i].displayName || i) + " (" + 
+                  plugins[i].version + " - " + 
+                  plugins[i].lastChange + ")";
+      FARR.emitResult(querykey, title, plugins[i].aliasstr, 
+                      plugins[i].icon || iconfilename, ALIAS, 
+                      MATCH_AGAINST_SEARCH, 99, plugins[i].aliasstr);
+    }
+    FARR.setState(querykey, STOPPED);
+    return;
+  }
+  // execute search() for each plugin
+  for (var i in plugins) {
+    try {
+      var isExplicit = queryraw.indexOf(plugins[i].aliasstr) == 0;
+      plugins[i].search(querykey, isExplicit, queryraw, querynokeyword, 
+                        modifier, triggermethod);
+    } catch(e) { 
+      error("plugin " + i + " has failed on search : " + 
+            "message: " + e.message + " " + "name: " + e.name);
+    }
+  }
+  FARR.setState(querykey, STOPPED);
+}
+//
+function onProcessTrigger(path, title, groupname, pluginid, thispluginid, 
+                          score, entrytype, args) {
+  for (var i in plugins) {
+    try {
+      var r = plugins[i].trigger(path, title, groupname, pluginid, thispluginid, 
+                                 score, entrytype, args);
+      if ((r & 3) != 0)
+        return r; // stop if stop or closed is required
+      } catch(e) { 
+        error("plugin " + i + " has failed on trigger : " + 
+              "message: " + e.message + " " + "name: " + e.name);
+      }
+  }
+}
+//
+function onReceiveKey(key, altpressed, controlpressed, shiftpressed) {
+  for (var i in plugins) {
+    try {
+      var r = plugins[i].receivekey(key, 
+                                    altpressed, controlpressed, shiftpressed);
+      if (r) { return true; }
+    } catch(e) { 
+      error("plugin " + i + " has failed on received key : " + 
+            "message: " + e.message + " " + "name: " + e.name);
+    }
+  }
+}
+
+function onWin32Message(wparam, lparam) {
+  var SHOW_SUBPLUGIN_CONFIG = 0;
+  var RELOAD_SUBPLUGINCONFIG = 1;
+  if (wparam == SHOW_SUBPLUGIN_CONFIG) {
+    FARR.debug("" + lparam);
+    var index = 0;
+    for (var i in plugins) {
+      if (index == lparam - 1) break;
+      index++;
+    }
+    plugins[i].showSettings();
+  } else if (wparam == RELOAD_SUBPLUGINCONFIG) {
+    var index = 0;
+    for (var i in plugins) {
+      if (index == lparam - 1) break;
+      index++;
+    }
+    plugins[i].settingsChanged();
+  }
+}
+
+function onDoAdvConfig() {  
+  try {
+    fso.DeleteFile(g_currentDirectory + "\\FSSCSettings.ini");
+  } catch(e) {
+  }
+  // extract plugins data for the settings
+  var index = 1;
+  for (var i in plugins) {
+    FARR.setIniValue(g_currentDirectory + "\\FSSCSettings.ini", index, 
+                     "PluginName", i);
+    FARR.setIniValue(g_currentDirectory + "\\FSSCSettings.ini", index, 
+                     "Icon", plugins[i].icon);
+    index++;
+  }
+  FARR.exec(g_currentDirectory + "\\FSSCSettings.exe", "", g_currentDirectory);
+  return true;
+}
+
 function onDoShowReadMe() {
 }
+
 function onSetStrValue(name, value) {
 }
+
 function onGetStrValue(name) {
 }
